0225ce053a67c1a268d352b23c6149a9000a9a04
tusmasoma <s.k18@icloud.com>
2024-06-05 16:59:23 +0900

    feat: add ddl.sql

---
+++ repository/mysql/init/ddl.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE IF NOT EXISTS `connecthubdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `connecthubdb` ;
+
+DROP TABLE IF EXISTS Users CASCADE;
+DROP TABLE IF EXISTS Messages CASCADE;
+DROP TABLE IF EXISTS ActionTags CASCADE;
+DROP TABLE IF EXISTS Rooms CASCADE;
+DROP TABLE IF EXISTS Workspaces CASCADE;
+
+CREATE TABLE Users (
+    id CHAR(36) PRIMARY KEY, -- UUIDは36文字の文字列として格納されます
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL  -- 暗号化されたパスワードを格納
+);
+
+CREATE TABLE Messages (
+    id CHAR(36) PRIMARY KEY, -- UUIDは36文字の文字列として格納されます
+    user_id CHAR(36) NOT NULL,
+    room_id CHAR(36) NOT NULL,
+    action_tag_id  CHAR(36) NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE,
+    FOREIGN KEY (room_id) REFERENCES Rooms(id) ON DELETE CASCADE,
+    FOREIGN KEY (action_tag_id) REFERENCES ActionTags(id) ON DELETE CASCADE
+);
+
+CREATE TABLE ActionTags (
+    id CHAR(36) PRIMARY KEY, -- UUIDは36文字の文字列として格納されます
+    name VARCHAR(50) NOT NULL,
+    description TEXT
+);
+
+CREATE TABLE Rooms (
+    id CHAR(36) PRIMARY KEY, -- UUIDは36文字の文字列として格納されます
+    workspace_id CHAR(36) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    private BOOLEAN NOT NULL,
+    description TEXT,
+    FOREIGN KEY (workspace_id) REFERENCES Workspaces(id) ON DELETE CASCADE
+);
+
+CREATE TABLE  Workspaces (
+    id CHAR(36) PRIMARY KEY, -- UUIDは36文字の文字列として格納されます
+    name VARCHAR(50) NOT NULL,
+    description TEXT
+);
