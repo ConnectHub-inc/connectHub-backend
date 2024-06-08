582a9d87215e33ef1a1b10782bfcb64115bfdf40
tusmasoma <s.k18@icloud.com>
2024-06-08 20:52:16 +0900

    feat: add log level

--- go.mod
+++ go.mod
@@ -40,6 +40,7 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/slack-go/slack v0.13.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ internal/log/log.go
@@ -0,0 +1,189 @@
+package log
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+//nolint:gochecknoglobals
+var (
+	SeverityDefault  = slog.LevelInfo
+	SeverityDebug    = slog.LevelDebug
+	SeverityInfo     = slog.LevelInfo
+	SeverityNotice   = slog.Level(2)
+	SeverityWarning  = slog.LevelWarn
+	SeverityError    = slog.LevelError
+	SeverityCritical = slog.Level(4)
+)
+
+// logger is the global logger.
+// it is initialized by init() and should not be modified.
+var logger *slog.Logger
+
+// Slack webhook URL
+var slackWebhookURL = os.Getenv("SLACK_WEBHOOK_URL")
+
+// init initializes the logger.
+func init() {
+	logFormat := os.Getenv("LOG_FORMAT")
+	handler := newHandler(logFormat)
+	logger = slog.New(handler)
+}
+
+// newHandler returns a slog.Handler based on the given format.
+func newHandler(format string) slog.Handler {
+	switch format {
+	case "json":
+		return slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level:       SeverityDefault,
+			ReplaceAttr: attrReplacerForDefault,
+		})
+	}
+
+	return slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level:       SeverityDefault,
+		ReplaceAttr: attrReplacerForDefault,
+	})
+}
+
+// attrReplacerForDefault is default attribute replacer.
+func attrReplacerForDefault(groups []string, attr slog.Attr) slog.Attr {
+	level, ok := attr.Value.Any().(slog.Level)
+	if ok {
+		attr.Value = toLogLevel(level)
+	}
+	return attr
+}
+
+// toLogLevel converts a slog.Level to a slog.Value.
+func toLogLevel(level slog.Level) slog.Value {
+	ls := "DEFAULT"
+
+	switch level {
+	case SeverityDebug:
+		ls = "DEBUG"
+	case SeverityInfo:
+		ls = "INFO"
+	case SeverityNotice:
+		ls = "NOTICE"
+	case SeverityWarning:
+		ls = "WARNING"
+	case SeverityError:
+		ls = "ERROR"
+	case SeverityCritical:
+		ls = "CRITICAL"
+	}
+
+	return slog.StringValue(ls)
+}
+
+// sendSlackNotification sends a message to the configured Slack webhook URL.
+func sendSlackNotification(level, msg string, attrs ...any) {
+	if slackWebhookURL == "" {
+		return
+	}
+	text := fmt.Sprintf("[%s] %s", level, msg)
+	for _, attr := range attrs {
+		text += fmt.Sprintf("\n%v", attr)
+	}
+	err := slack.PostWebhook(slackWebhookURL, &slack.WebhookMessage{
+		Text: text,
+	})
+	if err != nil {
+		fmt.Printf("Failed to send Slack notification: %v\n", err)
+	}
+}
+
+// Debug logs a debug message.
+func Debug(msg string, attrs ...any) {
+	DebugContext(context.Background(), msg, attrs...)
+}
+
+// DebugContext logs a debug message with a context.
+func DebugContext(ctx context.Context, msg string, attrs ...any) {
+	logger.Log(ctx, SeverityDebug, msg, attrs...)
+}
+
+// Info logs an info message.
+func Info(msg string, attrs ...any) {
+	InfoContext(context.Background(), msg, attrs...)
+}
+
+// InfoContext logs an info message with a context.
+func InfoContext(ctx context.Context, msg string, attrs ...any) {
+	logger.Log(ctx, SeverityInfo, msg, attrs...)
+}
+
+// Notice logs a notice message.
+func Notice(msg string, attrs ...any) {
+	NoticeContext(context.Background(), msg, attrs...)
+}
+
+// NoticeContext logs a notice message with a context.
+func NoticeContext(ctx context.Context, msg string, attrs ...any) {
+	logger.Log(ctx, SeverityNotice, msg, attrs...)
+}
+
+// Warn logs a warning message.
+func Warn(msg string, attrs ...any) {
+	WarnContext(context.Background(), msg, attrs...)
+}
+
+// WarnContext logs a warning message with a context.
+func WarnContext(ctx context.Context, msg string, attrs ...any) {
+	logger.Log(ctx, SeverityWarning, msg, attrs...)
+}
+
+// Error logs an error message.
+func Error(msg string, attrs ...any) {
+	ErrorContext(context.Background(), msg, attrs...)
+	sendSlackNotification("ERROR", msg, attrs...)
+}
+
+// ErrorContext logs an error message with a context.
+func ErrorContext(ctx context.Context, msg string, attrs ...any) {
+	logger.Log(ctx, SeverityError, msg, attrs...)
+	sendSlackNotification("ERROR", msg, attrs...)
+}
+
+// Critical logs a critical message.
+func Critical(msg string, attrs ...any) {
+	CriticalContext(context.Background(), msg, attrs...)
+	sendSlackNotification("CRITICAL", msg, attrs...)
+}
+
+// CriticalContext logs a critical message with a context.
+func CriticalContext(ctx context.Context, msg string, attrs ...any) {
+	logger.Log(ctx, SeverityCritical, msg, attrs...)
+	sendSlackNotification("CRITICAL", msg, attrs...)
+}
+
+// Panic logs a critical message and panics.
+func Panic(msg string, attrs ...any) {
+	PanicContext(context.Background(), msg, attrs...)
+	panic(msg)
+}
+
+// PanicContext logs a critical message with a context and panics.
+func PanicContext(ctx context.Context, msg string, attrs ...any) {
+	logger.Log(ctx, SeverityCritical, msg, attrs...)
+	sendSlackNotification("CRITICAL", msg, attrs...)
+	panic(msg)
+}
+
+// Fatal logs a critical message and exits.
+func Fatal(msg string, attrs ...any) {
+	FatalContext(context.Background(), msg, attrs...)
+	os.Exit(1)
+}
+
+// FatalContext logs a critical message with a context and exits.
+func FatalContext(ctx context.Context, msg string, attrs ...any) {
+	logger.Log(ctx, SeverityCritical, msg, attrs...)
+	sendSlackNotification("CRITICAL", msg, attrs...)
+	os.Exit(1)
+}
