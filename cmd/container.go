a2d9de91629badc45ac639ecf41f6b72ae351dc1
tusmasoma <s.k18@icloud.com>
2024-06-19 21:11:43 +0900

    chore: add provider

--- cmd/container.go
+++ cmd/container.go
@@ -45,6 +45,7 @@ func BuildContainer(ctx context.Context) (*dig.Container, error) {
 		redis.NewPubSubRepository,
 		usecase.NewUserUseCase,
 		usecase.NewMessageUseCase,
+		usecase.NewAuthUseCase,
 		handler.NewWebsocketHandler,
 		handler.NewUserHandler,
 		middleware.NewAuthMiddleware,
