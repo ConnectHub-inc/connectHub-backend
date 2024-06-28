b8bd27f6f539366414a8435badf0c079d5a14166
tusmasoma <s.k18@icloud.com>
2024-06-28 18:39:58 +0900

    chore: add db field

--- entity/room.go
+++ entity/room.go
@@ -3,10 +3,10 @@ package entity
 import "github.com/google/uuid"
 
 type Room struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Private     bool   `json:"private"`
+	ID          string `json:"id" db:"id"`
+	Name        string `json:"name" db:"name"`
+	Description string `json:"description" db:"description"`
+	Private     bool   `json:"private" db:"private"`
 }
 
 func NewRoom(name string, description string, private bool) *Room {
--- entity/user.go
+++ entity/user.go
@@ -3,11 +3,11 @@ package entity
 import "github.com/google/uuid"
 
 type User struct {
-	ID              string `json:"id"`
-	Name            string `json:"name"`
-	Email           string `json:"email"`
-	Password        string `json:"password"`
-	ProfileImageURL string `json:"profile_image_url"`
+	ID              string `json:"id" db:"id"`
+	Name            string `json:"name" db:"name"`
+	Email           string `json:"email" db:"email"`
+	Password        string `json:"password" db:"password"`
+	ProfileImageURL string `json:"profile_image_url" db:"profile_image_url"`
 	IsAdmin         bool   `json:"is_admin"`
 }
 
